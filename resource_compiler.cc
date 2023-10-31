6163b0a218b4db572efae061f21275c5c05f8ace
Kim Se-yeon <tpdussla93@gmail.com>
2023-10-31 22:55:08 +0900

    link nlohmann_json to resource_compiler

--- resource_compiler.cc
+++ resource_compiler.cc
@@ -1,7 +1,16 @@
 #include <iostream>
 
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
 int main() {
-    std::cout << "Resource Compiler" << std::endl;
+    json j = {
+        {"happy", true},
+        {"pi", 3.141},
+    };
+
+    std::cout << j.dump(4) << std::endl;
 
     return 0;
 }
