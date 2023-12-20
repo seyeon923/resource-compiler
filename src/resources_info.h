1310079dd17879eb2e3887564168d7fbd3d836de
Kim Se-yeon <tpdussla93@gmail.com>
2023-12-20 09:48:47 +0900

    Make "define_name" key be optional in a resources json

--- src/resources_info.h
+++ src/resources_info.h
@@ -5,6 +5,8 @@
 #include <vector>
 #include <fstream>
 #include <filesystem>
+#include <algorithm>
+#include <cctype>
 
 #include <nlohmann/json.hpp>
 
@@ -28,6 +30,21 @@ public:
           resource_path(std::move(resource_path)) {}
 };
 
+inline std::string KeyToDefineName(const std::string& key) {
+    std::string define_name(key.size() + 1, '_');
+
+    std::transform(std::begin(key), std::end(key), std::begin(define_name) + 1,
+                   [](char ch) {
+                       if (std::isalnum(static_cast<unsigned char>(ch))) {
+                           return static_cast<char>(
+                               std::toupper(static_cast<unsigned char>(ch)));
+                       } else {
+                           return '_';
+                       }
+                   });
+    return define_name;
+}
+
 inline std::vector<ResourceInfo> GetResourcesInfo(
     const std::string& json_path) {
     auto resources_base_dir = fs::path{json_path}.parent_path();
@@ -47,30 +64,36 @@ inline std::vector<ResourceInfo> GetResourcesInfo(
     }
 
     std::vector<ResourceInfo> ret;
-    for (auto& [key, resource] : resources_it->items()) {
-        const std::string resource_id{"resources[" + key + "]"};
+    for (auto& [idx, resource] : resources_it->items()) {
+        const std::string resource_id{"resources[" + idx + "]"};
+
+        std::string key, define_name, resource_path;
 
         auto key_it = resource.find("key");
         if (key_it == std::end(resource)) {
             throw std::runtime_error("Cannot find \"key\" entry from " +
                                      resource_id);
         }
+        key = key_it->get<std::string>();
+
         auto define_name_it = resource.find("define_name");
         if (define_name_it == std::end(resource)) {
-            throw std::runtime_error("Cannot find \"define_name\" entry from " +
-                                     resource_id);
+            define_name = KeyToDefineName(key);
+        } else {
+            define_name = define_name_it->get<std::string>();
         }
+
         auto resource_path_it = resource.find("resource_path");
         if (resource_path_it == std::end(resource)) {
             throw std::runtime_error(
                 "Cannot find \"resource_path\" entry from" + resource_id);
         }
+        resource_path = fs::canonical(resources_base_dir /
+                                      resource_path_it->get<fs::path>())
+                            .string();
 
-        ret.emplace_back(key_it->get<std::string>(),
-                         define_name_it->get<std::string>(),
-                         fs::canonical(resources_base_dir /
-                                       resource_path_it->get<fs::path>())
-                             .string());
+        ret.emplace_back(std::move(key), std::move(define_name),
+                         std::move(resource_path));
     }
     return ret;
 }
